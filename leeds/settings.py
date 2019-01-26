d47f6806de72e5ddeb5428580e61beb0fa2892fa
iSolution <qaisar.elit@gmail.com>
2019-01-26 10:27:39 +0500

    changes
    are made

--- leeds/settings.py
+++ leeds/settings.py
@@ -127,6 +127,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
 from django.urls import reverse_lazy
 
 LOGIN_REDIRECT_URL = reverse_lazy('dashboard')
--- user/views.py
+++ user/views.py
@@ -1,7 +1,29 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+from .models import Profile
+from .forms import UserRegistrationForm, UserEditForm, ProfileEditForm
+
+
+def register(request):
+     if request.method == 'POST':
+          user_form=UserRegistrationForm(request.POST)
+
+          if user_form.is_valid():
+               new_user = user_form.save(commit=False)
+               new_user.set_password(user_form.cleaned_data['password'])
+               profile = Profile.objects.create(user=new_user)
+               new_user.save()
+               return render(request, 'user/registration_done.html', {'new_user': new_user})
+     else:
+          user_form = UserRegistrationForm()
+
+     return render(request, 'registration/register.html', {'user_form': user_form})
+
+
+
+
 
 @login_required()
 def dashboard(request):
-     return render(request, 'user/home.html')
+     return render(request, 'user/dashboard.html', {'section': 'dashboard'})
