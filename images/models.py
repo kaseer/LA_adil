d3c8bfa58e6d60335d9dda956f80071d109fdab9
Qaisar <qaisar.elit@gmail.com>
2019-01-27 15:45:19 +0500

    images

--- images/models.py
+++ images/models.py
@@ -2,19 +2,16 @@ from django.db import models
 from django.conf import settings
 from django.utils.text import slugify
 from django.urls import reverse
-# Create your models here.
 
 
 class Image(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='images_created', on_delete=models.CASCADE)
-
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, blank=True)
     url = models.URLField()
     image = models.ImageField(upload_to='images/%Y/%m/%d')
     description = models.TextField(blank=True)
     created = models.DateTimeField(auto_now_add=True, db_index=True)
-
     users_like = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='images_liked', blank=True)
 
     def __str__(self):
@@ -27,4 +24,3 @@ class Image(models.Model):
 
     def get_absolute_url(self):
         return reverse('images:detail', args=[self.id, self.slug])
-
--- images/urls.py
+++ images/urls.py
@@ -1,11 +1,11 @@
-from django.urls import path
+from django.conf.urls import url
 from . import views
 
+
 urlpatterns = [
-    path('create/', views.image_create, name='create'),
-    path('detail<int:id>/<slug:slug>', views.image_detail, name='detail'),
-    path('like/', views.image_like, name='like'),
+    url(r'^create/$', views.image_create, name='create'),
+    url(r'^detail/(?P<id>\d+)/(?P<slug>[-\w]+)/$', views.image_detail, name='detail'),
+    url(r'^like/$', views.image_like, name='like'),
+    url(r'^$', views.image_list, name='list'),
 ]
-
-
 
